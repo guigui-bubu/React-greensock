39ada1b4a79d02bce8c86b1277dc206db441e7da
guigui-bubu <guillaumebuaud@yahoo.fr>
2021-05-26 15:11:41 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React, {useState, useRef, useEffect} from 'react';
+import { gsap} from 'gsap';
+
 
 function App() {
+
+  const [toggle, setToggle] = useState(false); // on le fait démarrer à false
+
+  const changeState = () => {   // fonction qui sert à changer le state, setToggle inverse de toggle
+    setToggle(!toggle)          // fonction qu'on passe à notre carte quand on veut cliker dessus
+  }
+
+  const cardRef = useRef(null) // Nous permet de sélectionner un element du DOM ( ex: la 'div' 'card'), retourne un Objet avec la propriete Current
+ 
+
+  useEffect(()=>{               
+    toggle ? 
+    gsap.to(cardRef.current, {
+      scale: 1.5,
+      rotate: 360,
+      duration: 1.2
+    })
+    :
+    gsap.to(cardRef.current, {
+      scale : 1,
+      rotate : 0,
+      duration : 1.2
+    })
+  }, [toggle]) // [] surveille mon state toggle, et dès qu'il change le useEffet s'exécute
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card" onClick={changeState} ref={cardRef}>
+      NEW YORK
+    </div>
     </div>
   );
 }
